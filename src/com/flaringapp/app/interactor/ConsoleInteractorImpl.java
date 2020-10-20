cc73502bd914dfdee99d647063e0ef7ae04caca4
Mallivance <ashpek01@gmail.com>
2020-10-20 18:32:06 +0300

    Refactor file structure

--- src/com/flaringapp/app/interactor/ConsoleInteractorImpl.java
+++ src/com/flaringapp/app/interactor/ConsoleInteractorImpl.java
@@ -6,9 +6,7 @@ import java.util.Scanner;
 
 public class ConsoleInteractorImpl implements ConsoleInteractor {
 
-    private final Scanner scanner = new Scanner(System.in);
-
-    private static final String options = "\n" +
+    private static final String OPTIONS = "\n" +
             "1. Sort by name\n" +
             "2. Sort by coffee volume\n" +
             "3. Sort by coffee sort rating\n" +
@@ -18,9 +16,11 @@ public class ConsoleInteractorImpl implements ConsoleInteractor {
             "- Sort: <number> <order>. Order can be \"asc\" or \"desc\")\n" +
             "- Filter: <number> <param>.\n";
 
+    private final Scanner scanner = new Scanner(System.in);
+
     @Override
     public void askWhatToDo(Callback<String> callback) {
-        System.out.println(options);
+        System.out.println(OPTIONS);
         String input = scanner.nextLine();
         callback.call(input);
     }
